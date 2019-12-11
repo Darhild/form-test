b6dbc6c34d0b5f24ae51d03a525899f5468a6798
Исаева Мария <isaeva-mariya@yandex.ru>
2019-12-11 17:00:34 +0300

    added autocomplete block

--- client/src/App.vue
+++ client/src/App.vue
@@ -16,8 +16,6 @@ export default {
 </script>
 
 <style lang="scss">
-@import url('https://fonts.googleapis.com/css?family=Roboto&display=swap');
-
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- client/src/components/Forminput.vue
+++ client/src/components/Forminput.vue
@@ -1,25 +1,40 @@
 <template>
-<div class="Input">
-  <input id="search" class="Input-Field" type="text" v-model="text"/>
-  <label for="search" class="Input-Label"></label>
-</div>
+  <div class="Input">
+    <div class="Input-Wrapper">
+      <input id="search" class="Input-Field" type="text" v-model="searchText"/>
+      <label for="search" class="Input-Label"></label>
+    </div>
+    <div class="Input-Autocomplete Autocomplete" v-if="wordsForAutocomplete.length !== 0">
+      <div class="Autocomplete-Item" v-for="item in wordsForAutocomplete" :key="item">
+        {{ item }}
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 import axios from 'axios';
+import { SERVER_URL } from './../env';
 
 export default {
   data() {
     return {
-      text: '',
+      searchText: '',
+      autocompleteData: [],
       wordsForAutocomplete: [],
       errors: [],
     }
   },
 
+  watch: {
+    searchText(value) {
+      this.wordsForAutocomplete = this.autocompleteData.filter(item => item.indexOf(value) !== -1);
+    }
+  },
+
   mounted() {
-    axios.get('https://desolate-mesa-06273.herokuapp.com/api')
-    .then(res => this.wordsForAutocomplete = res)
+    axios.get(`${SERVER_URL}/api`)
+    .then(res => this.autocompleteData = res.data)
     .catch(e => this.errors.push(e))
   }
 }
@@ -27,40 +42,43 @@ export default {
 
 <style lang="scss">
 .Input {
-  position: relative;
   width: 276px;
-  padding: 7px 0;
   margin-right: auto;
   margin-left: auto;
   font-family: 'Roboto', sans-serif;
   font-size: 13px;
 
-  &:before {
-    content: '';
-    display: block;
-    position: absolute;
-    bottom: 0;
-    left: 0;
-    width: 100%;
-    height: 1px;
-    background-color: #999;
-  }
+  &-Wrapper {
+    position: relative;
+    padding: 7px 0;
 
-  &:focus:before {
-    display: none;
-  }
+    &:before {
+      content: '';
+      display: block;
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      width: 100%;
+      height: 1px;
+      background-color: #999;
+    }
 
-  &:after {
-    content: '';
-    display: block;
-    position: absolute;
-    top: 5px;
-    right: 0;
-    width: 10px;
-    height: 10px;
-    transform: rotate(45deg);
-    border-right: 1px solid #858585;
-    border-bottom: 1px solid #858585;
+    &:focus:before {
+      display: none;
+    }
+
+    &:after {
+      content: '';
+      display: block;
+      position: absolute;
+      top: 5px;
+      right: 0;
+      width: 10px;
+      height: 10px;
+      transform: rotate(45deg);
+      border-right: 1px solid #858585;
+      border-bottom: 1px solid #858585;
+    }
   }
 
   &-Field {
