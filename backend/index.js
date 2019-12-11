fe682389e6087e203151db87a27083c5f552c83c
Исаева Мария <isaeva-mariya@yandex.ru>
2019-12-11 13:54:23 +0300

    disabled cors

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,11 @@ const json = require('./data.json');
 const app = express();
 const { PORT } = require('./env');
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
+
 app.get('/api', (req, res) => {
     res.send(json.data);
 })
