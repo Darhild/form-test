06f440196442760be889c487819557a12b33ac33
Исаева Мария <isaeva-mariya@yandex.ru>
2019-12-12 16:44:14 +0300

    added heroku postbuild

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const json = require('./data.json');
 const app = express();
 const { PORT } = require('./env');
@@ -6,6 +7,12 @@ const { PORT } = require('./env');
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   next();
+});
+
+app.use(express.static(path.resolve(__dirname, "../client/dist")));
+
+app.get("*", function(req, res) {
+  response.sendFile(path.resolve(__dirname, "../client/dist", "index.html"));
 });
 
 app.get('/api', (req, res) => {
